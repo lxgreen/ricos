889cf7e633a8f0cb452a748d68876c916f572a1b
danarish4 <danari@wix.com>
2021-08-17 11:43:59 +0300

    try to remove externals

--- e2e/test-env/webpack.config.js
+++ e2e/test-env/webpack.config.js
@@ -115,7 +115,7 @@ const config = [
       publicPath: '/static/',
     },
     target: 'node',
-    externals: [nodeExternals({ whitelist: [/.css/, /^wix-rich-content/, /^ricos-/] })],
+    // externals: [nodeExternals({ whitelist: [/.css/, /^wix-rich-content/, /^ricos-/] })],
     plugins: [
       new MiniCssExtractPlugin({
         filename: '[name].css',
