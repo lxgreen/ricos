af6e85000b814d4ef3fec9ca10dbaf5542f1bd4c
Oded Soffrin <odeds@wix.com>
2021-08-26 15:01:57 +0300

    jest fix try

---
+++ packages/ricos-editor/web/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  globals: {
+    NODE_ENV: 'test',
+  },
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.jsx?$': '<rootDir>/../../../babel.jest.monorepo.js',
+    '\\.st\\.css?$': require.resolve('@stylable/jest'),
+  },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(.*?\\.st\\.css$))', // libraries publish .st.css files in their dist
+  ],
+};
