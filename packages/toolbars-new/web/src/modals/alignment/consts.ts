bdd6adb7776d320d8c869ada605d9c986168274d
tamrat bayeh <tamratb@wix.com>
2021-09-13 11:35:09 +0300

    add line-spacing consts-n

---
+++ packages/toolbars-new/web/src/modals/alignment/consts.ts
@@ -0,0 +1,28 @@
+import { AlignTextCenterIcon, AlignJustifyIcon, AlignLeftIcon, AlignRightIcon } from '../../icons';
+
+export default [
+  {
+    text: 'AlignObject_Left_Tooltip',
+    tooltip: 'AlignObject_Left_Tooltip',
+    commandKey: 'left',
+    icon: AlignLeftIcon,
+  },
+  {
+    text: 'AlignObject_Center_Tooltip',
+    tooltip: 'AlignObject_Center_Tooltip',
+    commandKey: 'center',
+    icon: AlignTextCenterIcon,
+  },
+  {
+    text: 'AlignObject_Right_Tooltip',
+    tooltip: 'AlignObject_Right_Tooltip',
+    commandKey: 'right',
+    icon: AlignRightIcon,
+  },
+  {
+    text: 'AlignTextJustifyButton_Tooltip',
+    tooltip: 'AlignTextJustifyButton_Tooltip',
+    commandKey: 'justify',
+    icon: AlignJustifyIcon,
+  },
+];
