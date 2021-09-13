1adedebd89eaef8b037724b775a831f4dfa9010f
Matan Harsat <mharsat@users.noreply.github.com>
2021-09-13 12:08:58 +0300

    [tiptap] add link preview (#2914)

--- packages/plugin-link-preview/web/src/tiptap/component.tsx
+++ packages/plugin-link-preview/web/src/tiptap/component.tsx
@@ -4,11 +4,12 @@ import { LinkPreviewPluginEditorConfig } from '../types';
 import { PluginProps } from 'wix-rich-content-editor-common';
 import { LINK_PREVIEW_TYPE } from 'ricos-content';
 
-export const LinkPreview: React.FC<PluginProps> = ({ context, updateAttributes }) => {
+export const LinkPreview: React.FC<PluginProps> = ({ context, componentData }) => {
   const { isMobile, theme, iframeSandboxDomain, config } = context;
   const settings: LinkPreviewPluginEditorConfig = config[LINK_PREVIEW_TYPE] || {};
   const blockProps = {
     setFocusToBlock: () => null,
+    getData: () => componentData,
   };
 
   return (
