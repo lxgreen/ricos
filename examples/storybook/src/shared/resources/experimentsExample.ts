d41591111a7c8aa9ab1814813eda07113c519997
noamtwix <45332991+noamtwix@users.noreply.github.com>
2021-10-18 09:28:02 +0300

    [common] fix: scrollIntoView mobile chrome bug (#2993)

--- examples/storybook/src/shared/resources/experimentsExample.ts
+++ examples/storybook/src/shared/resources/experimentsExample.ts
@@ -10,6 +10,7 @@ export const EXPERIMENTS = [
   { name: 'removeUsmFromImageUrls', scope: 'ricos' },
   { name: 'lazyImagesAndIframes', scope: 'ricos' },
   { name: 'anchorScrollWithoutScrollIntoView', scope: 'ricos' },
+  { name: 'anchorScrollWithSmoothScrollIntoViewIfNeeded', scope: 'ricos' },
   { name: 'useSrcSet', scope: 'ricos' },
   { name: 'pastedFilesSupport', scope: 'ricos' },
   { name: 'normalizePoll', scope: 'ricos' },
