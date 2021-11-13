1d51611b015b5ab543abc2006b7534b67d2f14fd
yaron nachshon <yaronn@wix.com>
2021-11-13 08:19:15 +0200

    Perf/optimize fullscreen modal (#3011)

--- examples/storybook/src/shared/resources/experimentsExample.ts
+++ examples/storybook/src/shared/resources/experimentsExample.ts
@@ -6,7 +6,6 @@ export const EXPERIMENTS = [
   { name: 'newSocialEmbedModal', scope: 'ricos' },
   { name: 'forceIframeSandboxDomain', scope: 'ricos' },
   { name: 'useSeo', scope: 'wixRicos' },
-  { name: 'tableCellsLimit', scope: 'ricos', input: [100, 200, 300, 500, 1000] },
   { name: 'spoilerInSettings', scope: 'ricos' },
   { name: 'removeUsmFromImageUrls', scope: 'ricos' },
   { name: 'lazyImagesAndIframes', scope: 'ricos' },
@@ -15,4 +14,5 @@ export const EXPERIMENTS = [
   { name: 'useSrcSet', scope: 'ricos' },
   { name: 'pastedFilesSupport', scope: 'ricos' },
   { name: 'normalizePoll', scope: 'ricos' },
+  { name: 'optimizeFullScreenModal', scope: 'ricos' },
 ];
