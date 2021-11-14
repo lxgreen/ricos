7a0c2d013e0edf0ff7a5cd3825abe23db002e6cf
yaron nachshon <yaronn@wix.com>
2021-11-14 17:32:57 +0200

    WIP2

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,66 @@
 const globby = require('globby');
 const fs = require('fs-extra');
 const path = require('path');
-
+const ts = require('typescript');
+const { flattenDeep, union } = require('lodash');
 const getFolderNamePkgMap = packageJsonFiles => {
-  const folderNamePkgMap = {};
+  const folderNamePkgMap = [];
   packageJsonFiles.forEach(pkgJsonPath => {
     const absolutePath = path.resolve(pkgJsonPath);
     const pkgJson = fs.readJSONSync(absolutePath);
     const splitedPaths = absolutePath.split(path.sep);
     const packageFolderName = splitedPaths[splitedPaths.length - 3]; // ex: packages/ricos/web/package.json
-    folderNamePkgMap[packageFolderName] = packageFolderName;
+    folderNamePkgMap.push({
+      folderAbsolutePath: path.dirname(absolutePath),
+      folderName: packageFolderName,
+      packageName: packageFolderName,
+    });
   });
   return folderNamePkgMap;
+};
+
+const writeFileTsFile = (folderName, references = []) => {
+  const tsConfigTemplate = fs.readFileSync(path.join(__dirname, 'tsconfig.template'));
+  const referenceStr = references.map(reference => {
+    return `{
+              "path": "../../web/${reference}"
+          }`;
+  });
+  const tsConfigFile = tsConfigTemplate.toString().replace('${references}', referenceStr);
+  fs.writeFileSync(`${folderName}/tsconfig.json`, tsConfigFile);
+  //   console.log(tsConfigTemplate.toString().replace('${references}', referenceStr));
+};
+
+const getImportFromPackage = async package => {
+  const files = await globby([
+    `packages/${package.folderName}/web/src/**/*.{ts, tsx}`,
+    `packages/${package.folderName}/web/lib/**/*.{ts, tsx}`,
+  ]);
+  const totalImports = [];
+  files.forEach(file => {
+    const filePath = path.resolve(file);
+    const imports = ts
+      .preProcessFile(fs.readFileSync(filePath, 'utf8'), true, true)
+      .importedFiles.map(({ fileName }) => fileName);
+    totalImports.push(imports);
+  });
+
+  return union(flattenDeep(totalImports));
+};
+
+const getReferencesFromPackage = (packages, imports) => {
+  const foundPkgs = [];
+  //   console.log(imports);
+  union(imports).forEach(importItem => {
+    const pkgs = packages.find(pkg => {
+      //   console.log(importItem);
+      return importItem.includes(`${pkg.packageName}`) && importItem.indexOf('..') === -1;
+    });
+    if (pkgs) {
+      foundPkgs.push(pkgs);
+    }
+  });
+  return foundPkgs;
 };
 
 const build = async () => {
@@ -20,8 +69,20 @@ const build = async () => {
     { realpath: true }
   );
   const pkgFolderNameMap = {};
-  const folderNamePkgMap = getFolderNamePkgMap(packageJsonFiles);
-  console.log(folderNamePkgMap);
+  const packages = getFolderNamePkgMap(packageJsonFiles);
+  const singlePkg = packages[5];
+  console.log(singlePkg);
+  packages.forEach(async pkg => {
+    const imports = await getImportFromPackage(pkg);
+    const references = getReferencesFromPackage(packages, imports);
+    writeFileTsFile(
+      pkg.folderAbsolutePath,
+      union(references.map(reference => reference.packageName))
+    );
+  });
+
+  //   packages.forEach(packages => {
+  //   });
 };
 
 build();
