6553853c9cb4aa731f3365bfe89b5744253452ea
danarish4 <danari@wix.com>
2021-11-14 11:13:28 +0200

    [tiptap] fix insert node command

--- packages/tiptap-editor/web/src/core/commands/insertNode.ts
+++ packages/tiptap-editor/web/src/core/commands/insertNode.ts
@@ -14,6 +14,6 @@ declare module '@tiptap/core' {
 }
 
 export const insertNode: RawCommands['insertNode'] = (type, attrs = {}) => ({ commands }) => {
-  const { id, rest } = toTiptap(attrs);
+  const { id, ...rest } = toTiptap(attrs);
   return commands.insertContent({ type, attrs: { id: id || generateId(), ...rest } });
 };
