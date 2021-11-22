fa39334de87cfabce0dd23559aa8a1895b4d3fcc
Barak <barakl@wix.com>
2021-11-22 15:04:56 +0200

    [ricos-content] feature: metadata-utils (#3147)

---
+++ packages/ricos-content/web/src/RicosContentAPI/createEmptyContent.spec.ts
@@ -0,0 +1,23 @@
+import { RichContent } from 'ricos-schema';
+import { createEmptyContent } from './createEmptyContent';
+
+describe('createEmptyContent', () => {
+  it('should create valid empty content', () => {
+    const date = new Date();
+    const newContent = createEmptyContent({
+      version: 1,
+      id: '1234',
+      createdTimestamp: date,
+      updatedTimestamp: date,
+    });
+    expect(newContent).toStrictEqual<RichContent>({
+      nodes: [],
+      metadata: {
+        version: 1,
+        id: '1234',
+        createdTimestamp: date,
+        updatedTimestamp: date,
+      },
+    });
+  });
+});
