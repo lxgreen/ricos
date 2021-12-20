042e5525faa55043f20006059276b4af06b112f0
tamrat-bayeh <54495324+tamrat-bay@users.noreply.github.com>
2021-12-20 09:52:20 +0200

    [ui-components] add: selectionlistitem component (#3279)

--- packages/plugin-social-polls/web/src/components/settings/preset-selector/PollPresetSelector.jsx
+++ packages/plugin-social-polls/web/src/components/settings/preset-selector/PollPresetSelector.jsx
@@ -2,7 +2,11 @@
 import React, { PureComponent } from 'react';
 import { merge } from 'lodash';
 
-import { SelectionList, SettingsSeparator } from 'wix-rich-content-ui-components';
+import {
+  SelectionList,
+  SettingsSeparator,
+  SelectionListItem,
+} from 'wix-rich-content-ui-components';
 import { mergeStyles } from 'wix-rich-content-common';
 
 import { getRandomValue } from '../../../helpers';
@@ -50,11 +54,8 @@ export class PollPresetSelector extends PureComponent {
     helpers.closeModal();
   };
 
-  renderOption = ({ item }) => (
-    <>
-      <item.icon />
-      <p className={this.styles.selectionListOptionLabel}>{item.label}</p>
-    </>
+  renderOption = ({ item, selected }) => (
+    <SelectionListItem icon={<item.icon />} label={item.label} selected={selected} />
   );
 
   render() {
