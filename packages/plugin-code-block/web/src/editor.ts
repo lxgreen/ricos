a95b7ee5f68edd088ec696746d5df7f1e8964a37
Dana Rish <45333005+danarish4@users.noreply.github.com>
2021-12-21 13:08:24 +0200

    [tiptap] move code block extension to ricos side (#3251)
    * [tiptap] move code block extension to ricos side
    
    * remove @tiptap/extension-code-block from yarn.lock
    
    * move to code block package
    
    * cleanup
    
    * import code styles

--- packages/plugin-code-block/web/src/editor.ts
+++ packages/plugin-code-block/web/src/editor.ts
@@ -2,12 +2,16 @@ import { createCodeBlockPlugin } from './createCodeBlockPlugin';
 import { CODE_BLOCK_TYPE, CodeBlockPluginEditorConfig } from './types';
 import { DEFAULTS } from './defaults';
 import { EditorPluginCreator } from 'wix-rich-content-common';
+import { createTiptapExtensions } from './tiptap';
+import { TiptapEditorPlugin } from 'ricos-tiptap-types';
 
 export const pluginCodeBlock: EditorPluginCreator<CodeBlockPluginEditorConfig> = config => {
+  const pluginConfig = { ...DEFAULTS.config, ...config };
   return {
-    config: { ...DEFAULTS.config, ...config },
+    config: pluginConfig,
     type: CODE_BLOCK_TYPE,
     createPlugin: createCodeBlockPlugin,
     ModalsMap: {},
-  };
+    tiptapExtensions: createTiptapExtensions(pluginConfig),
+  } as TiptapEditorPlugin;
 };
