7130c185725f3d83c71108ea92d26b5ae5324c2e
tamrat bayeh <tamratb@wix.com>
2021-12-23 10:59:22 +0200

    code-refactor

--- examples/storybook/stories/Theme/wixStyle.ts
+++ examples/storybook/stories/Theme/wixStyle.ts
@@ -40,7 +40,7 @@ export const withWixStyle: WithStyle = theme =>
           borderRadius: '8px',
           boxShadow: '0 8px 8px 0 rgba(22, 45, 61, 0.12), 0 3px 24px 0 rgba(22, 45, 61, 0.18)',
         },
-        colorPalettebButton: {
+        colorPaletteButton: {
           borderRadius: '6px',
         },
         colorPaletteSelectedButton: {
