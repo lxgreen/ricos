c2f371f25f4584c386b8a1cfa6be255dededc6b9
Shaul Goral <shaulgo@gmail.com>
2022-01-12 13:28:09 +0200

    [*] use azure for cache (#3374)

--- lage.config.js
+++ lage.config.js
@@ -5,20 +5,16 @@ module.exports = {
     test: ['build:test'],
     ci: [],
   },
+  // Ignores these minimatch patterns when considers what packages have changed for the --since flag
+  // ignore: ['CAHNGELOG.md'],
   cacheOptions: {
     writeRemoteCache: true,
-    cacheStorageConfig: {
-      provider: 'npm',
-      options: {
-        npmPackageName: 'ricos-build-cache',
-        npmrcUserconfig: process.env.PWD + '/.lageNpmConfig',
-      },
-    },
     // These are the subset of files in the package directories that will be saved into the cache
     outputGlob: ['dist/**', 'lib/**', 'generated/**'],
     // These are relative to the git root, and affects the hash of the cache
     // Any of these file changes will invalidate cache
-    environmentGlob: ['../../../yarn.lock'],
+    // environmentGlob: ['../../../yarn.lock'],
+    internalCacheFolder: '.cache/backfill',
   },
   npmClient: 'yarn',
 };
