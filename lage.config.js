3a5ede17ecc494d2cdcece4a59e9b698d3a5e8ba
shaulg <shaulgo@gmail.com>
2022-04-10 14:18:41 +0300

    [cache] update globals

--- lage.config.js
+++ lage.config.js
@@ -13,12 +13,7 @@ module.exports = {
     outputGlob: ['dist/**', 'lib/**', 'generated/**'],
     // These are relative to the git root, and affects the hash of the cache
     // Any of these file changes will invalidate cache
-    environmentGlob: [
-      '../../../yarn.lock',
-      '../../../rollup.plugins.ts',
-      '../../../rollup.externals.ts',
-      '../../../rollup.config.common.ts',
-    ],
+    environmentGlob: ['./rollup.plugins.ts', './rollup.externals.ts', './rollup.config.common.ts'],
     internalCacheFolder: '../../../.cache/backfill',
   },
   npmClient: 'yarn',
