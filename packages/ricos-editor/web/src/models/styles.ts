2e0690c135f0b060ea341924822fb070483f3561
maimonav <maimonav@post.bgu.ac.il>
2022-05-17 16:11:07 +0300

    [ricos-editor] update styles.ts interfaces

--- packages/ricos-editor/web/src/models/styles.ts
+++ packages/ricos-editor/web/src/models/styles.ts
@@ -1,25 +1,29 @@
 import type { ReactElement } from 'react';
-import type { DocumentStyle, Decoration } from 'ricos-schema';
+import type {
+  DocumentStyle as RichContentDocumentStyle,
+  Decoration,
+  Decoration_Type,
+} from 'ricos-schema';
 import type { RicosTheme } from 'ricos-types';
 
-type StyleType = keyof Required<DocumentStyle>;
+type StyledNodeType = keyof Required<DocumentStyle>;
+
+export interface TextualTheme {
+  getDecoration(type: StyledNodeType, decoration: Decoration_Type): Decoration;
+  toStyleTag(): ReactElement;
+  setTheme(theme: RicosTheme): TextualTheme;
+}
+
+export interface DocumentStyle {
+  getDecoration(type: StyledNodeType, decoration: Decoration_Type): Decoration;
+  toStyleTag(): ReactElement;
+  toContent(): RichContentDocumentStyle;
+  setStyle(type: StyledNodeType, decorations: Decoration[]): DocumentStyle;
+  setDocumentStyle(documentStyle: RichContentDocumentStyle): DocumentStyle;
+}
 
 export interface Styles {
-  isActive(type: StyleType, decoration: Decoration): boolean;
-  getThemeStyles(): ThemeStyles;
-  getDocumentStyles(): DocumentStyles;
-}
-
-export interface ThemeStyles {
-  isActive(type: StyleType, decoration: Decoration): boolean;
-  setTheme(theme: RicosTheme): ThemeStyles;
-  toStyleTag(): ReactElement;
-}
-
-export interface DocumentStyles {
-  isActive(type: StyleType, decoration: Decoration): boolean;
-  setDocumentStyles(documentStyle: DocumentStyle): DocumentStyles;
-  setStyle(type: StyleType, decorations: Decoration[]): DocumentStyles;
-  toStyleTag(): ReactElement;
-  toContent(): DocumentStyle;
+  getDecoration(type: StyledNodeType, decoration: Decoration_Type): Decoration;
+  getTheme(): TextualTheme;
+  getDocumentStyle(): DocumentStyle;
 }
