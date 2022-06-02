ea7c02b08ddf483da68e66b5faa7ce787f33887d
Dana Rish <45333005+danarish4@users.noreply.github.com>
2022-06-02 10:44:19 +0300

    [example-app]editor settings: ui (#3968)

--- examples/main/src/Components/EditorSettings.tsx
+++ examples/main/src/Components/EditorSettings.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import type { FC } from 'react';
 import type { TestAppConfig } from '../types';
@@ -14,9 +16,9 @@ interface Props {
   setEditorSettings: (settings: TestAppConfig) => void;
 }
 
-const Toggle = ({ onChange, checked, label }) => (
-  <span style={{ display: 'flex' }}>
-    <input tabIndex={0} type={'checkbox'} onChange={onChange} checked={checked} />
+const Toggle = ({ onChange, checked, label, type = 'checkbox' }) => (
+  <span className="checkbox">
+    <input tabIndex={0} type={type} onChange={onChange} checked={checked} />
     {label}
   </span>
 );
@@ -62,178 +64,138 @@ const EditorSettings: FC<Props> = ({ editorSettings = {}, setEditorSettings }) =
   const isSocialExposeButtonCheck = (type, buttonName) =>
     editorSettings?.pluginsConfig?.[type]?.exposeEmbedButtons?.includes(buttonName);
 
+  const isHorizontalMenu = editorSettings?.toolbarConfig?.addPluginMenuConfig?.horizontalMenuLayout;
+
   return (
-    <div style={{ padding: 20 }}>
-      <h3>Plugins toolbar settings:</h3>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <Toggle
-          label="use horizontal menu"
-          checked={editorSettings?.toolbarConfig?.addPluginMenuConfig?.horizontalMenuLayout}
-          onChange={() =>
-            setPluginMenuConfig({
-              tablePluginMenu: false,
-              horizontalMenuLayout: true,
-              showSearch: false,
-              splitToSections: false,
-            })
-          }
-        />
-        <Toggle
-          label="use plugin menu"
-          checked={!editorSettings?.toolbarConfig?.addPluginMenuConfig?.horizontalMenuLayout}
-          onChange={() =>
-            setPluginMenuConfig({ tablePluginMenu: true, horizontalMenuLayout: false })
-          }
-        />
-        <Toggle
-          label="show search"
-          onChange={() =>
-            setPluginMenuConfig({
-              showSearch: !editorSettings?.toolbarConfig?.addPluginMenuConfig?.showSearch,
-            })
-          }
-          checked={editorSettings?.toolbarConfig?.addPluginMenuConfig?.showSearch}
-        />
-        <Toggle
-          label="split to section"
-          onChange={() =>
-            setPluginMenuConfig({
-              splitToSections: !editorSettings?.toolbarConfig?.addPluginMenuConfig?.splitToSections,
-            })
-          }
-          checked={editorSettings?.toolbarConfig?.addPluginMenuConfig?.splitToSections}
-        />
+    <div className="editorSettings">
+      <div>
+        <div className="sectionHeader">Plugin Menu</div>
+        <div className="pluginMenuToggle">
+          <div
+            className={`option ${!isHorizontalMenu && 'selected'}`}
+            onClick={() =>
+              setPluginMenuConfig({ tablePluginMenu: true, horizontalMenuLayout: false })
+            }
+          >
+            Plugin Menu
+          </div>
+          <div
+            className={`option ${isHorizontalMenu && 'selected'}`}
+            onClick={() =>
+              setPluginMenuConfig({
+                tablePluginMenu: false,
+                horizontalMenuLayout: true,
+                showSearch: false,
+                splitToSections: false,
+              })
+            }
+          >
+            Horizontal Menu
+          </div>
+        </div>
+        <div className="checkboxWrapper" style={{ height: 48 }}>
+          {!isHorizontalMenu && (
+            <>
+              <Toggle
+                label="show search"
+                onChange={() =>
+                  setPluginMenuConfig({
+                    showSearch: !editorSettings?.toolbarConfig?.addPluginMenuConfig?.showSearch,
+                  })
+                }
+                checked={editorSettings?.toolbarConfig?.addPluginMenuConfig?.showSearch}
+              />
+              <Toggle
+                label="split to section"
+                onChange={() =>
+                  setPluginMenuConfig({
+                    splitToSections:
+                      !editorSettings?.toolbarConfig?.addPluginMenuConfig?.splitToSections,
+                  })
+                }
+                checked={editorSettings?.toolbarConfig?.addPluginMenuConfig?.splitToSections}
+              />
+            </>
+          )}
+        </div>
       </div>
-
-      <h3>Plugins settings:</h3>
-      <h4>Vertical Embed:</h4>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <Toggle
-          label="slimLayout"
-          onChange={() =>
-            setPluginSettings(VERTICAL_EMBED_TYPE, {
-              slimLayout: !editorSettings?.pluginsConfig?.[VERTICAL_EMBED_TYPE]?.slimLayout,
-            })
-          }
-          checked={editorSettings?.pluginsConfig?.[VERTICAL_EMBED_TYPE]?.slimLayout}
-        />
+      <div>
+        <div className="sectionHeader">Plugin Layout</div>
+        <div className="checkboxWrapper">
+          <div className="pluginType">Vertical Embed</div>
+          <Toggle
+            type="radio"
+            label="Default Layout"
+            onChange={() => setPluginSettings(VERTICAL_EMBED_TYPE, { slimLayout: false })}
+            checked={!editorSettings?.pluginsConfig?.[VERTICAL_EMBED_TYPE]?.slimLayout}
+          />
+          <Toggle
+            type="radio"
+            label="Slim Layout"
+            onChange={() => setPluginSettings(VERTICAL_EMBED_TYPE, { slimLayout: true })}
+            checked={editorSettings?.pluginsConfig?.[VERTICAL_EMBED_TYPE]?.slimLayout}
+          />
+        </div>
       </div>
+      <div>
+        <div className="sectionHeader">Plugin Expose Buttons</div>
+        <div className="checkboxWrapper">
+          <div className="pluginType">Social Embed</div>
+          {['Instagram', 'TikTok', 'Twitter'].map(buttonName => (
+            <Toggle
+              key={buttonName}
+              label={buttonName}
+              onChange={() =>
+                setPluginSettings(LINK_PREVIEW_TYPE, {
+                  exposeEmbedButtons: toggleSocialExposeButton(LINK_PREVIEW_TYPE, buttonName),
+                })
+              }
+              checked={isSocialExposeButtonCheck([LINK_PREVIEW_TYPE], buttonName)}
+            />
+          ))}
 
-      <h4>Social Embed:</h4>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <div>Expose buttons:</div>
+          <div className="pluginType">Video</div>
+          {['video', 'youTube'].map(buttonName => (
+            <Toggle
+              key={buttonName}
+              label={buttonName}
+              onChange={() =>
+                setPluginSettings(VIDEO_TYPE, {
+                  exposeButtons: toggleExposeButton(VIDEO_TYPE, buttonName),
+                })
+              }
+              checked={isExposeButtonCheck([VIDEO_TYPE], buttonName)}
+            />
+          ))}
 
-        <Toggle
-          label="instagram"
-          onChange={() =>
-            setPluginSettings(LINK_PREVIEW_TYPE, {
-              exposeEmbedButtons: toggleSocialExposeButton(LINK_PREVIEW_TYPE, 'Instagram'),
-            })
-          }
-          checked={isSocialExposeButtonCheck([LINK_PREVIEW_TYPE], 'Instagram')}
-        />
-        <Toggle
-          label="tiktok"
-          onChange={() =>
-            setPluginSettings(LINK_PREVIEW_TYPE, {
-              exposeEmbedButtons: toggleSocialExposeButton(LINK_PREVIEW_TYPE, 'TikTok'),
-            })
-          }
-          checked={isSocialExposeButtonCheck([LINK_PREVIEW_TYPE], 'TikTok')}
-        />
+          <div className="pluginType">Html</div>
+          {['html', 'adsense'].map(buttonName => (
+            <Toggle
+              key={buttonName}
+              label={buttonName}
+              onChange={() =>
+                setPluginSettings(HTML_TYPE, {
+                  exposeButtons: toggleExposeButton(HTML_TYPE, buttonName),
+                })
+              }
+              checked={isExposeButtonCheck([HTML_TYPE], buttonName)}
+            />
+          ))}
 
-        <Toggle
-          label="twitter"
-          onChange={() =>
-            setPluginSettings(LINK_PREVIEW_TYPE, {
-              exposeEmbedButtons: toggleSocialExposeButton(LINK_PREVIEW_TYPE, 'Twitter'),
-            })
-          }
-          checked={isSocialExposeButtonCheck([LINK_PREVIEW_TYPE], 'Twitter')}
-        />
-      </div>
-      <h4>Video:</h4>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <div>Expose buttons:</div>
-
-        <Toggle
-          label="video"
-          onChange={() =>
-            setPluginSettings(VIDEO_TYPE, {
-              exposeButtons: toggleExposeButton(VIDEO_TYPE, 'video'),
-            })
-          }
-          checked={isExposeButtonCheck([VIDEO_TYPE], 'video')}
-        />
-
-        <Toggle
-          label="youtube"
-          onChange={() =>
-            setPluginSettings(VIDEO_TYPE, {
-              exposeButtons: toggleExposeButton(VIDEO_TYPE, 'youTube'),
-            })
-          }
-          checked={isExposeButtonCheck([VIDEO_TYPE], 'youTube')}
-        />
-      </div>
-      <h4>Html:</h4>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <div>Expose buttons:</div>
-
-        <Toggle
-          label="html"
-          onChange={() =>
-            setPluginSettings(HTML_TYPE, {
-              exposeButtons: toggleExposeButton(HTML_TYPE, 'html'),
-            })
-          }
-          checked={isExposeButtonCheck([HTML_TYPE], 'html')}
-        />
-
-        <Toggle
-          label="adsense"
-          onChange={() =>
-            setPluginSettings(HTML_TYPE, {
-              exposeButtons: toggleExposeButton(HTML_TYPE, 'adsense'),
-            })
-          }
-          checked={isExposeButtonCheck([HTML_TYPE], 'adsense')}
-        />
-      </div>
-
-      <h4>Audio:</h4>
-      <div style={{ display: 'grid', gridRowGap: 10 }}>
-        <div>Expose buttons:</div>
-
-        <Toggle
-          label="audio"
-          onChange={() =>
-            setPluginSettings(AUDIO_TYPE, {
-              exposeButtons: toggleExposeButton(AUDIO_TYPE, 'audio'),
-            })
-          }
-          checked={isExposeButtonCheck([AUDIO_TYPE], 'audio')}
-        />
-
-        <Toggle
-          label="soundCloud"
-          onChange={() =>
-            setPluginSettings(AUDIO_TYPE, {
-              exposeButtons: toggleExposeButton(AUDIO_TYPE, 'soundCloud'),
-            })
-          }
-          checked={isExposeButtonCheck([AUDIO_TYPE], 'soundCloud')}
-        />
-
-        <Toggle
-          label="spotify"
-          onChange={() =>
-            setPluginSettings(AUDIO_TYPE, {
-              exposeButtons: toggleExposeButton(AUDIO_TYPE, 'spotify'),
-            })
-          }
-          checked={isExposeButtonCheck([AUDIO_TYPE], 'spotify')}
-        />
+          <div className="pluginType">Audio</div>
+          {['audio', 'soundCloud', 'spotify'].map(buttonName => (
+            <Toggle
+              key={buttonName}
+              label={buttonName}
+              onChange={() =>
+                setPluginSettings(AUDIO_TYPE, {
+                  exposeButtons: toggleExposeButton(AUDIO_TYPE, buttonName),
+                })
+              }
+              checked={isExposeButtonCheck([AUDIO_TYPE], buttonName)}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
